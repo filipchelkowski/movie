caded7f4c950633a0288c557772c489a01c76758
filipchelkowski <filip.chelkowski@gmail.com>
2026-04-26 17:20:02 +0200

    HTMLs added

--- movie/views.py
+++ movie/views.py
@@ -5,12 +5,12 @@ from .forms import MovieForm
 
 def movie_list(request):
     movies = Movie.objects.all().order_by('-id')
-    return render(request, 'movie/movie_list.html', {'movies': movies})
+    return render(request, 'movies/movie_list.html', {'movies': movies})
 
 
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
-    return render(request, 'movie/movie_detail.html', {'movie': movie})
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
 
 
 def movie_create(request):
@@ -23,7 +23,7 @@ def movie_create(request):
         else:
         form = MovieForm()
 
-        return render(request, 'movie/movie_form.html', {'form': form})
+        return render(request, 'movies/movie_form.html', {'form': form})
 
 
 def movie_update(request, pk):
@@ -38,7 +38,7 @@ def movie_update(request, pk):
         else:
             form = MovieForm(instance=movie)
 
-            return render(request, 'movie/movie_form.html', {'form': form})
+            return render(request, 'movies/movie_form.html', {'form': form})
 
 
 def movie_delete(request, pk):
@@ -48,4 +48,4 @@ def movie_delete(request, pk):
         movie.delete()
         return redirect('movie_list')
 
-    return render(request, 'movie/movie_confirm_delete.html', {'movie': movie})
+    return render(request, 'movies/movie_confirm_delete.html', {'movie': movie})
